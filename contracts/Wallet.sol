f777eaf03e12e4f503cc9c0fea180ae54b600321
deployAt <mikowhy@tutanota.com>
2020-11-18 20:03:17 +0100

    feat: basic Yearn Wallet implementation

--- contracts/Wallet.sol
+++ contracts/Wallet.sol
@@ -3,10 +3,51 @@ pragma solidity ^0.6.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
+//https://github.com/iearn-finance/itoken/blob/master/contracts/YDAIv3.sol#L409
+interface IYDAI {
+    function deposit(uint256 _amount) external;
+
+    function withdraw(uint256 _shares) external;
+
+    function balanceOf(address account) external view returns (uint256);
+
+    function getPricePerFullShare() external view returns (uint256);
+}
+
 contract Wallet {
     address admin;
+    IERC20 dai = IERC20(0x6B175474E89094C44Da98b954EedeAC495271d0F);
+    IYDAI yDai = IYDAI(0xC2cB1040220768554cf699b0d863A3cd4324ce32);
 
     constructor() public {
         admin = msg.sender;
+    }
+
+    function save(uint256 amount) external {
+        dai.transferFrom(msg.sender, address(this), amount);
+        _save(amount);
+    }
+
+    function spend(uint256 amount, address recipient) external {
+        require(msg.sender == admin, "only admin");
+        uint256 balanceShares = yDai.balanceOf(address(this));
+        yDai.withdraw(balanceShares);
+        dai.transfer(recipient, amount);
+        uint256 balanceDai = dai.balanceOf(address(this));
+
+        if (balanceDai > 0) {
+            _save(amount);
+        }
+    }
+
+    function _save(uint256 amount) internal {
+        dai.approve(address(yDai), amount);
+        yDai.deposit(amount);
+    }
+
+    function balance() external view returns (uint256) {
+        uint256 price = yDai.getPricePerFullShare();
+        uint256 balanceShares = yDai.balanceOf(address(this));
+        return price * balanceShares;
     }
 }
